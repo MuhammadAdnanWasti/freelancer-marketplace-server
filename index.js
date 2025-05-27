aa94389b2cc6c8b2429f47ceacd6210055a850e1
muhammad-adnan <mohammadadnanwasti@gmail.com>
2025-05-27 19:45:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors=require('cors')
+const app = express()
+require('dotenv').config()
+
+const port =process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pd9mpbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World freelancers!')
+})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
